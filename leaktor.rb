675222a02a047eff5607ae5cb083ab606f2c8f07
Jonas Resch <reschjonas@users.noreply.github.com>
2026-02-08 10:47:34 +0100

    update leaktor to v0.3.0: switch to rustls-tls, drop openssl dependency

--- leaktor.rb
+++ leaktor.rb
@@ -2,13 +2,11 @@ class Leaktor < Formula
   desc "Secrets scanner with pattern matching, entropy analysis, and live validation"
   homepage "https://github.com/reschjonas/leaktor"
   url "https://github.com/reschjonas/leaktor/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "01448ab3ebcce77bf30ae8891358e17ad35bb24d622e5170c1dd3517eab92370"
+  sha256 "365b2e20de6702f0b31ef042105fa50c1f96cbbbb48cd72870420a8e3e61bb35"
   license "MIT"
   head "https://github.com/reschjonas/leaktor.git", branch: "master"
 
   depends_on "rust" => :build
-  depends_on "pkg-config" => :build
-  depends_on "openssl@3"
 
   def install
     system "cargo", "install", *std_cargo_args
